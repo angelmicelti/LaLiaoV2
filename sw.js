b5fa591c341a9bd3a763c549b804f30050bcc76a
angelmicelti <angelmicelti@users.noreply.github.com>
2026-09-20 20:54:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker para LaLiao V2
 // Cachea los recursos principales para funcionamiento offline
 
-const CACHE_NAME = 'laliao-v2-v5';
+const CACHE_NAME = 'laliao-v2-v6';
 const ASSETS = [
   './',
   './index.html',
